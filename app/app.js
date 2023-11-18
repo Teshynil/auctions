f1dbef2e668c4b82a24dcb46edd77b2a8f61fe77
Carlos Adrian <Teshynil@users.noreply.github.com>
2023-11-18 16:34:46 -0600

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,204 @@
+const encodeDetails = function (name, duration, minimalPrice, basePrice) {
+    const details = JSON.stringify({ name, duration, minimalPrice, basePrice });
+    return btoa(details); // Encoding to Base64
+};
+
+const decodeDetails = function (encodedDetails) {
+    const decoded = atob(encodedDetails); // Decoding from Base64
+    return JSON.parse(decoded);
+};
+
+(function () {
+
+    function getUsername(msg) {
+        let username = '';
+
+        while (!username || username.trim() === '') {
+            username = prompt(msg);
+        }
+
+        return username;
+    }
+
+    const socket = io('ws://localhost:8585');
+
+    window.username = ''; // Store the registered username
+    window.intervalId = null;
+    const urlParams = new URLSearchParams(window.location.search);
+
+    // Function to retrieve the stored cookie from localStorage
+    function getStoredCookie() {
+        return localStorage.getItem('userSession');
+    }
+
+    // Function to store the received cookie in localStorage
+    function storeCookie(cookie) {
+        localStorage.setItem('userSession', cookie);
+    }
+
+    socket.on('connect', () => {
+        const storedCookie = getStoredCookie(); // Retrieve the stored cookie from local storage or other means
+        
+        if (!urlParams.has('id')) {
+            socket.emit('checkAuth', storedCookie, urlParams.get('id'));
+            const auctionModal = new bootstrap.Modal(document.getElementById('auctionModal'));
+            auctionModal.show();
+
+            document.getElementById('submitDetails').onclick = () => {
+                const auctionName = document.getElementById('auctionName').value.trim();
+                const auctionDuration = document.getElementById('auctionDuration').value;
+                const auctionMinimalPrice = document.getElementById('auctionMinimalPrice').value;
+                const auctionBasePrice = document.getElementById('auctionBasePrice').value;
+
+                if (auctionName === '') {
+                    alert('Por favor proporciona un nombre valido.');
+                    return;
+                }
+
+                const encodedDetails = encodeDetails(auctionName, auctionDuration, auctionMinimalPrice, auctionBasePrice);
+                window.location.href = `${window.location.origin}${window.location.pathname}?id=${encodedDetails}`;
+            };
+        }else{
+            
+            socket.emit('auth', storedCookie, urlParams.get('id'));
+        }
+    });
+
+    socket.on('reconnectSuccess', () => {
+        // Handle successful reconnection (update UI or perform other actions)
+        console.log('Successfully reconnected!');
+
+    });
+
+    socket.on('newCookie', (newCookie) => {
+
+        console.log('Received new cookie:', newCookie);
+        storeCookie(newCookie); // Store the new cookie for future use
+
+        window.username = getUsername('Ingresa tu nombre para participar en la subasta');
+        socket.emit('register', { name: username, auctionId: urlParams.get('id') });
+    });
+
+    socket.on('registrationSuccess', (username, isMaster) => {
+        window.isMaster = isMaster;
+        document.getElementById('username').innerText = `Nombre: ${username}`;
+        const decodeAuctionDetails = decodeDetails(urlParams.get('id'));
+        window.price = decodeAuctionDetails.basePrice;
+        window.duration = decodeAuctionDetails.duration;
+        window.item = decodeAuctionDetails.name;
+        document.getElementById('item').innerText = window.item;
+        window.minimalPrice = decodeAuctionDetails.minimalPrice;
+        if (window.isMaster) {
+            document.querySelector('.controls-section').hidden = false;
+        }
+        updateFlipClock();
+
+    });
+
+    socket.on('auctionAlreadyStarted', () => {
+        document.getElementById('username').innerText = `${window.username}`;
+    });
+
+    socket.on('retryRegister', () => {
+        window.username = getUsername('Usuario ya registrado usa otro nombre');
+        socket.emit('register', { name: window.username, auctionId: urlParams.get('id') });
+    });
+
+    socket.on('tick', (newPrice) => {
+        if (window.intervalId == null) {
+            window.price = newPrice;
+            updateFlipClock();
+        }
+    });
+
+    socket.on('startingAuction', () => {
+        if (!window.isMaster) {
+            document.querySelector('.bid-section').hidden = false;
+        }
+    });
+
+    socket.on('wrongAuction', (auctionId) => {
+        alert('Sigues conectado a otra subasta primero desconectate.');
+        window.location.href = `${window.location.origin}${window.location.pathname}?id=${auctionId}`;
+    });
+
+    socket.on('endAuction', (soldTo, soldBy) => {
+        if (window.isMaster) {
+            clearInterval(window.intervalId);
+            window.intervalId = null;
+        }
+        document.querySelector('.bid-section').hidden = false;
+        document.getElementById('endAuctionBtn').hidden = true;
+        document.getElementById('endAuctionBtn').hidden = true;
+        document.getElementById('price-clock').addEventListener("transitionend", () => {
+            document.getElementById('sellTo').hidden = false;
+            document.getElementById('sellTo').innerHTML = `Vendido a: ${soldTo}`;
+            document.getElementById('sellBy').hidden = false;
+            document.getElementById('sellBy').innerHTML = `Por ${soldBy}`;
+        });
+    });
+
+    socket.on('updateParticipants', (participants) => {
+        const participantsList = document.getElementById('participants');
+        participantsList.innerHTML = ''; // Clear the list before updating
+
+        // Update the list of participants excluding the master
+        participants.forEach(participant => {
+            if (participant !== window.username) {
+                const li = document.createElement('li');
+                li.innerText = participant;
+                participantsList.appendChild(li);
+            }
+        });
+    });
+
+    if (window.isMaster) {
+        document.querySelector('.controls-section').hidden = false;
+    }
+
+    document.getElementById('username').addEventListener('click', () => {
+        window.username = getUsername('Ingresa tu nombre para participar en la subasta');
+        socket.emit('register', { name: username, auctionId: urlParams.get('id') });
+    });
+
+    document.getElementById('exitAuctionBtn').addEventListener('click', () => {
+        socket.emit('exitAuction');
+        window.location.href = `${window.location.origin}${window.location.pathname}`;
+    });
+
+    document.getElementById('endAuctionBtn').addEventListener('click', () => {
+        socket.emit('endAuction', window.price);
+    });
+
+    document.getElementById('startAuctionBtn').addEventListener('click', () => {
+        if (window.isMaster) {
+            document.querySelector('.controls-section').hidden = true;
+            socket.emit('startAuction');
+            setTimeout(() => {
+                window.reduction = (Math.trunc(((window.price - window.minimalPrice) / window.duration) * 100) / 100).toFixed(2);;
+                let count = 0;
+                window.intervalId = setInterval(() => {
+                    count++;
+                    if (count == duration) {
+                        window.price = window.minimalPrice;
+                        updateFlipClock();
+                        socket.emit('tick', window.price);
+                    }
+                    if (count >= duration) {
+                        socket.emit('endAuction', window.username);
+                        clearInterval(window.intervalId);
+                        return;
+                    }
+                    window.price -= window.reduction;
+                    updateFlipClock();
+                    socket.emit('tick', window.price);
+                }, 1000);
+            }, 3000);
+        }
+    });
+})();
+
+
+function updateFlipClock() {
+    document.getElementById('price-clock').style.setProperty("--price", window.price);
+}
---
+++ server/index.mjs
@@ -0,0 +1,181 @@
+// Import necessary modules and set up the server
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import { v4 as uuidv4 } from 'uuid'; // Import UUID generator
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: { origin: "*" }
+});
+
+const emit = function (socket, ev, ...args) {
+  console.log(`Emiting ${ev} to ${socket.id} with data: ${args}`);
+  socket.emit(ev, ...args);
+}
+
+const broadcast = function (room, ev, ...args) {
+  console.log(`Broadcasting ${ev} to room ${room} with data: ${args}`);
+  io.to(room).emit(ev, ...args);
+}
+
+const userSessions = {};
+const registeredUsers = {};
+const auctions = {};
+
+function getUserIdFromCookie(cookie) {
+  const userId = parseCookie(cookie);
+  return userId ?? false;
+}
+
+function parseCookie(cookie) {
+  try {
+    const cookieData = {}; // Placeholder for parsed data
+    const parts = cookie.split(';');
+    for (const part of parts) {
+      const [key, value] = part.trim().split('=');
+      cookieData[key] = value;
+    }
+    return cookieData['userId'];
+  } catch {
+    return null;
+  }
+}
+
+function generateNewCookie() {
+  const userId = uuidv4();
+  const expirationDate = new Date();
+  expirationDate.setDate(expirationDate.getDate() + 1); // Expires in 1 day
+  const expirationDateString = expirationDate.toUTCString();
+  const newCookie = `userId=${userId}; expires=${expirationDateString}; path=/`;
+  return newCookie;
+}
+
+function getParticipants(auctionId) {
+  const participants = Object.values(registeredUsers)
+    .filter(user => user.auctionId === auctionId && !user.master)
+    .map(user => user.name);
+  return participants;
+}
+
+io.on('connection', (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on('register', ({ name, auctionId }) => {
+    console.log(`Register event received from ${socket.id} with name: ${name}`);
+    const userId = userSessions[socket.id];
+    const duplicateNames = Object.values(registeredUsers)
+      .filter(user => user.auctionId === auctionId && user.name === name);
+
+    if (duplicateNames.length > 0) {
+      emit(socket, 'retryRegister', true);
+      console.log(`Duplicated Name ${name}`);
+      return;
+    }
+
+    let isFirstRegister = Object.values(registeredUsers).findIndex(user => {
+      return user.id !== userId && user.auctionId === auctionId;
+    }) === -1;
+    if (!isFirstRegister && auctions[auctionId] !== null) {
+      isFirstRegister = auctions[auctionId].master == userId;
+    }
+    socket.join(auctionId);
+    if (isFirstRegister) {
+      console.log(`User ${socket.id} is the master for auctionId: ${auctionId}`);
+      auctions[auctionId] = { status: 0, master: userId };
+    }
+    if (auctions[auctionId].status == 0) {
+      registeredUsers[userId] = { id: userId, name, auctionId, master: isFirstRegister };
+    } else {
+      emit(socket, 'auctionAlreadyStarted', true);
+    }
+    emit(socket, 'registrationSuccess', name, isFirstRegister);
+    let participants = getParticipants(auctionId);
+    console.log(participants);
+    broadcast(auctionId, 'updateParticipants', participants);
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`User disconnected: ${socket.id}`);
+    delete userSessions[socket.id]
+  });
+
+  socket.on('exitAuction', () => {
+    console.log(`User exited the auction: ${socket.id}`);
+    const userId = userSessions[socket.id];
+    delete registeredUsers[userId];
+  });
+
+  socket.on('auth', (cookie, auctionId) => {
+    // Check if the received cookie is associated with any user
+    let userId = getUserIdFromCookie(cookie);
+
+    if (userId && registeredUsers[userId]) {
+      // Reconnect the client if the cookie is valid and associated with a user
+      registeredUsers[userId].socketId = socket.id;
+      if (auctionId == registeredUsers[userId].auctionId) {
+        socket.join(registeredUsers[userId].auctionId);
+
+        // Emit event to update participants after successful reconnection
+        const participants = getParticipants(registeredUsers[userId].auctionId);
+        broadcast(registeredUsers[userId].auctionId, 'updateParticipants', participants);
+
+        emit(socket, 'reconnectSuccess', true);
+        console.log(`Emit reconnectSuccess to ${socket.id}`);
+        emit(socket, 'registrationSuccess', registeredUsers[userId].name, registeredUsers[userId].master);
+        if (auctions[registeredUsers[userId].auctionId].status == 1) {
+          emit(socket, "startingAuction");
+        }
+      } else {
+        emit(socket, "wrongAuction", registeredUsers[userId].auctionId);
+      }
+    } else {
+      const newCookie = generateNewCookie();
+      userId = getUserIdFromCookie(newCookie);
+      emit(socket, 'newCookie', newCookie);
+      console.log(`Emit newCookie to ${socket.id}`);
+    }
+    userSessions[socket.id] = userId;
+  });
+
+  socket.on('checkAuth', (cookie, auctionId) => {
+    // Check if the received cookie is associated with any user
+    let userId = getUserIdFromCookie(cookie);
+
+    if (userId && registeredUsers[userId]) {
+      // Reconnect the client if the cookie is valid and associated with a user
+      registeredUsers[userId].socketId = socket.id;
+      if (auctionId != registeredUsers[userId].auctionId) {
+        emit(socket, "wrongAuction", registeredUsers[userId].auctionId);
+      }
+    }
+  });
+
+  socket.on('tick', (price) => {
+    try {
+      const userId = userSessions[socket.id];
+      broadcast(registeredUsers[userId].auctionId, 'tick', price);
+    } catch (error) {
+      console.log('Error registered:', error);
+    }
+  });
+
+  socket.on('startAuction', () => {
+    const userId = userSessions[socket.id];
+    auctions[registeredUsers[userId].auctionId].status = 1;
+    setTimeout(() => {
+      broadcast(registeredUsers[userId].auctionId, 'startingAuction');
+    }, 3000);
+  });
+
+  socket.on('endAuction', (price) => {
+    const userId = userSessions[socket.id];
+    auctions[registeredUsers[userId].auctionId].status = 0;
+    broadcast(registeredUsers[userId].auctionId, 'endAuction', registeredUsers[userId].name, price);
+  });
+
+});
+
+const PORT = 8585;
+httpServer.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
